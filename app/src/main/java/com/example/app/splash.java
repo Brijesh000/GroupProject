f9259044e44ec032fa152cfdaefb3cd87db73d0c
hp <bsoneji755@rku.ac.in>
2020-08-01 17:14:55 +0530

    splash screen & login page

---
+++ app/src/main/java/com/example/app/splash.java
@@ -0,0 +1,29 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //splash screen
+
+        new Handler().postDelayed(
+                new Runnable(){
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(splash.this, MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }, 5000
+        );
+    }
+}
